f68829d1e2d77fe0926ebb54a8feac010d4e3b5a
ukyi <devcat.js@gmail.com>
2022-09-03 22:52:47 +0900

    ✨ Setting 추가

---
+++ src/main/modules/setting.ts
@@ -0,0 +1,29 @@
+import { app, type Rectangle } from 'electron';
+import fs from 'fs';
+import { join } from 'path';
+
+type SettingData = {
+  bounds: Rectangle
+};
+
+const parseDataFile = (filePath: string, defaultSetting = {} as SettingData) => {
+  try {
+    return JSON.parse(fs.readFileSync(filePath).toString());
+  } catch (error) {
+    return defaultSetting;
+  }
+};
+
+export class Setting {
+  private dataPath = join(app.getPath('userData'), 'string.json');
+  private data = parseDataFile(this.dataPath) as SettingData;
+
+  get<K extends keyof SettingData>(key: K): SettingData[K] {
+    return this.data[key];
+  }
+
+  set<K extends keyof SettingData>(key: K, data: SettingData[K]) {
+    this.data[key] = data;
+    fs.writeFileSync(this.dataPath, JSON.stringify(this.data));
+  }
+}
