e349297f7644d2c2b43ff22f25491de8130eee98
ukyi <devcat.js@gmail.com>
2022-09-03 22:50:34 +0900

    ✨ GraphiQL 추가

--- src/renderer/pages/App.tsx
+++ src/renderer/pages/App.tsx
@@ -1,8 +1,12 @@
-import { useEffect, useState } from 'react';
-import '../assets/app.css';
+import { createGraphiQLFetcher } from '@graphiql/toolkit';
+import { GraphiQL } from 'graphiql';
+import { useEffect } from 'react';
+import 'graphiql/graphiql.css';
 
 export default function App() {
-  const [count, setCount] = useState(0);
+  const fetcher = createGraphiQLFetcher({
+    url: 'https://ytu-wine-api.netlify.app/v1/graphql',
+  });
 
   useEffect(() => {
     window.app.invoke('send-msg', 'test message').then(message => console.log('send-msg', message));
@@ -13,28 +17,5 @@ export default function App() {
     return () => window.app.off(replyId);
   }, []);
 
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src="vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank" rel="noreferrer">
-          <img src="react.svg" className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button type="button" onClick={() => setCount(count => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  );
+  return (<GraphiQL fetcher={fetcher} />);
 }
