4bf13bdc06838a2ae0a239ddc4cc658ff5202412
ukyi <devcat.js@gmail.com>
2022-09-03 22:53:40 +0900

    ✨ Module 데코레이터 추가

--- src/main/utils/decorators.ts
+++ src/main/utils/decorators.ts
@@ -1,7 +1,16 @@
-import { singleton, injectable, inject } from 'tsyringe';
+import { singleton, injectable, inject, registry, ClassProvider, InjectionToken, Provider, RegistrationOptions, injectAll, autoInjectable } from 'tsyringe';
 
 export const IPC_INVOKE = 'ipc:invoke';
-export const IPC_ON = 'ipc:on';
+export const IPC_SEND = 'ipc:send';
+export const CONTROLLERS = 'controllers';
+
+type ClassConstructor = ClassProvider<any>['useClass'];
+type RegistrationProvider = { token: InjectionToken; options?: RegistrationOptions; } & Provider;
+type ModuleProvider = ClassConstructor | RegistrationProvider;
+type RegistrationModule = {
+  controllers?: ModuleProvider[];
+  providers?: ModuleProvider[];
+};
 
 export const IpcInvoke = (event: string): MethodDecorator => {
   if (!event) throw new Error('ipc invoke event is required');
@@ -10,9 +19,20 @@ export const IpcInvoke = (event: string): MethodDecorator => {
 
 export const IpcSend = (event: string): MethodDecorator => {
   if (!event) throw new Error('ipc on event is required');
-  return (target, propertyName) => Reflect.defineMetadata(IPC_ON, event, target, propertyName);
+  return (target, propertyName) => Reflect.defineMetadata(IPC_SEND, event, target, propertyName);
 };
 
-export const Controller = singleton;
+export const Singleton = singleton;
 export const Injectable = injectable;
+export const AutoInjectable = autoInjectable;
 export const Inject = inject;
+export const InjectAll = injectAll;
+export const Registry = registry;
+
+const isProvider = <T extends RegistrationProvider>(provider: T | ClassConstructor): provider is T => !!(provider as T)?.token;
+const getProviders = (constructors?: ModuleProvider[], token?: InjectionToken): RegistrationProvider[] => constructors?.map(constructor => (isProvider(constructor) ? constructor : ({ token: token || constructor, useValue: constructor }))) ?? [];
+export const Module = (registration: RegistrationModule) => {
+  const providers = getProviders(registration.providers);
+  const controllers = getProviders(registration.controllers, CONTROLLERS);
+  return registry([...providers, ...controllers]);
+};
