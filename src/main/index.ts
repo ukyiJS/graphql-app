26daae3c04941cd0fbaf32a530e47f01513bca08
ukyi <devcat.js@gmail.com>
2022-09-04 20:19:47 +0900

    🔥 resolve CustomDecorator 제거

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,7 +1,5 @@
 import 'reflect-metadata';
 import { container } from 'tsyringe';
 import { AppModule } from './app.module';
-import { CustomDecorator } from './modules/customDecorator';
 
 container.resolve(AppModule);
-container.resolve(CustomDecorator);
