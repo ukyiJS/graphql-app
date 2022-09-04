fec7a19e73b594ae06525ff0a0b330d4966a3066
ukyi <devcat.js@gmail.com>
2022-09-04 22:02:44 +0900

    ✨ init 에러처리 추가

--- src/main/modules/mainApp.ts
+++ src/main/modules/mainApp.ts
@@ -1,42 +1,32 @@
 import { MainWindow } from '@main/modules/mainWindow';
-import { app, BrowserWindow } from 'electron';
-import { container } from 'tsyringe';
+import { Inject, Injectable } from '@main/utils/decorators';
+import { app } from 'electron';
 
+@Injectable()
 export class MainApp {
-  constructor() {
-    this.init();
+  constructor(@Inject(MainWindow) private mainWindow: MainWindow) {
+    this.init().catch(e => {
+      console.error('Failed create window:', e);
+      app.quit();
+    });
   }
 
-  init = () => {
-    app.on('window-all-closed', () => {
-      container.resolve(BrowserWindow).destroy();
-      if (process.platform !== 'darwin') app.quit();
-    });
+  init = async () => {
+    if (!app.requestSingleInstanceLock()) app.quit();
 
+    const window = await app.whenReady().then(this.mainWindow.create);
+    app.on('activate', () => window?.show());
+    app.on('window-all-closed', () => process.platform !== 'darwin' && app.quit());
     app.on('second-instance', () => {
-      const window = container.resolve(BrowserWindow);
       if (!window) return;
-
       if (window.isMinimized()) window.restore();
       window.focus();
     });
-
-    app.on('activate', () => {
-      container.resolve(BrowserWindow)?.show();
-    });
-
-    const isSingleInstance = app.requestSingleInstanceLock();
-    if (!isSingleInstance) {
-      app.quit();
-      process.exit(0);
-    }
 
     if (import.meta.env.DEV) {
       app.whenReady()
         .then(() => import('electron-debug').then(({ default: debug }) => debug({ showDevTools: false })))
         .then(() => import('../modules/devTools').then(({ DevTools }) => DevTools.install(DevTools.REACT)));
     }
-
-    app.whenReady().then(container.resolve(MainWindow).create);
   };
 }
