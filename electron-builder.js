ce250ab44b9962588d8c0ea36d8174ea9255404c
ukyi <devcat.js@gmail.com>
2022-09-04 22:34:24 +0900

    ✨ icon 추가

--- electron-builder.js
+++ electron-builder.js
@@ -5,10 +5,12 @@ const config = {
   productName: 'graphiql-app',
   appId: 'graphiql.app',
   files: ['dist/**/*'],
+  icon: 'icons/icon.ico',
   directories: {
     output: 'release/${version}',
   },
   mac: {
+    icon: 'icons/icon.icns',
     category: 'public.app-category.utilities',
     artifactName: '${productName}-${version}-${os}.${ext}',
   },
@@ -16,6 +18,7 @@ const config = {
     artifactName: '${productName}-${version}-${os}.${ext}',
   },
   dmg: {
+    icon: 'icons/icon.icns',
     artifactName: '${productName}-${version}.${ext}',
   },
   nsis: {
