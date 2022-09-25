9af0b76b69fa8452af2c81a6fb35aa6c43e8f697
ukyi <devcat.js@gmail.com>
2022-09-26 02:48:20 +0900

    ✨ queryClient, lazyMinLoadTime 추가

--- src/renderer/main.tsx
+++ src/renderer/main.tsx
@@ -1,8 +1,31 @@
-import React from 'react';
+import Loading from '@renderer/components/loading';
+import { lazyMinLoadTime } from '@renderer/utils/lazyMinLoadTime';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import 'graphiql/graphiql.css';
+import { Suspense } from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './pages/App';
-import './assets/index.css';
+import Modal from 'react-modal';
+import './styles/index.css';
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode><App /></React.StrictMode>,
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 30000,
+      suspense: true,
+    },
+  },
+});
+
+const App = lazyMinLoadTime(() => import('./pages/app'), 1500);
+
+const rootElement = document.getElementById('root') as HTMLElement;
+ReactDOM.createRoot(rootElement).render(
+  <QueryClientProvider client={queryClient}>
+    <Suspense fallback={<Loading />}>
+      <App />
+    </Suspense>
+    <ReactQueryDevtools initialIsOpen={false} position="bottom-right" />
+  </QueryClientProvider>,
 );
+Modal.setAppElement(rootElement);
