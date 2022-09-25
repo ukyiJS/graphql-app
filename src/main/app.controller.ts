f000a35e753daf8d90994eeef88013ff47383da2
ukyi <devcat.js@gmail.com>
2022-09-26 02:53:12 +0900

    ✨ getHistory, setHistory 추가

--- src/main/app.controller.ts
+++ src/main/app.controller.ts
@@ -1,27 +1,10 @@
 import { AppEmitterEvent } from '@main/modules/appEmitterEvent';
-import { Setting } from '@main/modules/setting';
-import { type BrowserWindow, type KeyboardEvent } from 'electron';
 import { AppService } from './app.service';
 import { Inject, Injectable, IpcInvoke, IpcSend, On } from './utils/decorators';
 
 @Injectable()
 export class AppController {
-  constructor(
-    @Inject(AppService) private appService: AppService,
-    @Inject(AppEmitterEvent) private event: AppEmitterEvent,
-    @Inject(Setting) private setting: Setting,
-  ) {}
-
-  @IpcSend('reply-msg')
-  replyMsg(msg: string) {
-    return `${this.appService.getDelayTime()} seconds later, the main process replies to your message: ${msg}`;
-  }
-
-  @IpcInvoke('send-msg')
-  async handleSendMsg(msg: string) {
-    this.replyMsg(msg);
-    return `The main process received your message: ${msg}`;
-  }
+  constructor(@Inject(AppService) private appService: AppService, @Inject(AppEmitterEvent) private event: AppEmitterEvent) {}
 
   @IpcSend('url-input-click')
   showUrlInput() {
@@ -30,16 +13,16 @@ export class AppController {
 
   @IpcInvoke('get-history')
   getHistory() {
-    return this.setting.get('history') ?? [];
+    return this.appService.getHistory();
   }
 
   @IpcInvoke('set-history')
   setHistory(history: string[]) {
-    return this.setting.set('history', history);
+    return this.appService.setHistory(history);
   }
 
   @On('url-input-click')
-  onUrlInputClick(window: BrowserWindow, event: KeyboardEvent) {
+  onUrlInputClick() {
     this.showUrlInput();
   }
 }
