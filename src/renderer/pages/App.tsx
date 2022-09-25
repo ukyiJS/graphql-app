8b186cb65dc9ea3c480bfdadff946d846738b3cc
ukyi <devcat.js@gmail.com>
2022-09-26 02:50:07 +0900

    ✨ useQuery history 추가

--- src/renderer/pages/App.tsx
+++ src/renderer/pages/App.tsx
@@ -1,21 +1,15 @@
 import { createGraphiQLFetcher } from '@graphiql/toolkit';
+import UrlInputModal from '@renderer/components/urlInputModal';
+import { useQuery } from '@tanstack/react-query';
 import { GraphiQL } from 'graphiql';
-import { useEffect } from 'react';
-import 'graphiql/graphiql.css';
 
 export default function App() {
-  const fetcher = createGraphiQLFetcher({
-    url: 'https://ytu-wine-api.netlify.app/v1/graphql',
-  });
-
-  useEffect(() => {
-    window.app.invoke('send-msg', 'test message').then(message => console.log('send-msg', message));
-  }, []);
-
-  useEffect(() => {
-    const replyId = window.app.on('reply-msg', message => console.log('### reply-msg', message));
-    return () => window.app.off(replyId);
-  }, []);
-
-  return (<GraphiQL fetcher={fetcher} />);
+  const { data: [history] = [] } = useQuery(['history'], async (): Promise<string[]> => window.app.invoke<string[]>('get-history'));
+  const fetcher = createGraphiQLFetcher({ url: history ?? 'https://localhost:3000' });
+  return (
+    <>
+      <UrlInputModal />
+      <GraphiQL fetcher={fetcher} />
+    </>
+  );
 }
