d317ef3e63fdff867fc85e6695a7437692a85813
ukyi <devcat.js@gmail.com>
2022-09-26 02:45:00 +0900

    ✨ EMITTER_ON 추가

--- src/main/utils/decorators.ts
+++ src/main/utils/decorators.ts
@@ -1,11 +1,24 @@
-import { autoInjectable, type ClassProvider, inject, injectable, injectAll, type InjectionToken, type Provider, type RegistrationOptions, registry, singleton, container } from 'tsyringe';
+import {
+  autoInjectable,
+  type ClassProvider,
+  inject,
+  injectable,
+  injectAll,
+  type InjectionToken,
+  type Provider,
+  type RegistrationOptions,
+  registry,
+  singleton,
+  container,
+} from 'tsyringe';
 
 export const IPC_INVOKE = 'ipc:invoke';
 export const IPC_SEND = 'ipc:send';
+export const EMITTER_ON = 'emitter:on';
 export const CONTROLLERS = 'controllers';
 
 type ClassConstructor = ClassProvider<any>['useClass'];
-type RegistrationProvider = { token: InjectionToken; options?: RegistrationOptions; } & Provider;
+type RegistrationProvider = { token: InjectionToken; options?: RegistrationOptions } & Provider;
 type ModuleProvider = ClassConstructor | RegistrationProvider;
 type RegistrationModule = {
   controllers?: ModuleProvider[];
@@ -13,14 +26,49 @@ type RegistrationModule = {
   imports?: InjectionToken[];
 };
 
+/**
+ * @example
+ * - @IpcInvoke('event')
+ * - method(message: string) {
+ * -   return message;
+ * - }
+ *
+ * - @IpcInvoke 데코레이터는 아래와 같음
+ * - ipcMain.handle('event', (event: IpcMainInvokeEvent, message: string) => message);
+ */
 export const IpcInvoke = (event: string): MethodDecorator => {
   if (!event) throw new Error('ipc invoke event is required');
   return (target, propertyName) => Reflect.defineMetadata(IPC_INVOKE, event, target, propertyName);
 };
 
+/**
+ * @example
+ * - @IpcSend('event')
+ * - method(message: string) {
+ * -   return message;
+ * - }
+ *
+ * - @IpcSend 데코레이터는 아래와 같음
+ * - new BrowserWindow().webContents.send('event', message);
+ */
 export const IpcSend = (event: string): MethodDecorator => {
   if (!event) throw new Error('ipc on event is required');
   return (target, propertyName) => Reflect.defineMetadata(IPC_SEND, event, target, propertyName);
+};
+
+/**
+ * @example
+ * - @On('event')
+ * - method(message: string) {
+ * -   return message;
+ * - }
+ *
+ * - @on 데코레이터는 아래와 같음
+ * - AppEmitterEvent.on('event', (message: string) => message);
+ */
+export const On = (event: string): MethodDecorator => {
+  if (!event) throw new Error('on event is required');
+  return (target, propertyName) => Reflect.defineMetadata(EMITTER_ON, event, target, propertyName);
 };
 
 export const Singleton = singleton;
@@ -35,10 +83,12 @@ const isProvider = <T extends RegistrationProvider>(provider: T | ClassConstruct
   if (isConstructor(provider)) return false;
   return !!provider.token;
 };
-const getProviders = (constructors?: ModuleProvider[]): RegistrationProvider[] => constructors?.map(constructor => {
-  if (isProvider(constructor)) return constructor;
-  return { token: constructor, useClass: constructor };
-}) ?? [];
+const getProviders = (constructors?: ModuleProvider[]): RegistrationProvider[] =>
+  constructors?.map(constructor => {
+    if (isProvider(constructor)) return constructor;
+    return { token: constructor, useClass: constructor };
+  }) ?? [];
+
 export const Module = (registration: RegistrationModule) => {
   registration.controllers?.forEach(controller => container.register(CONTROLLERS, { useValue: controller }));
   if (registration?.imports) registration.imports.forEach(_import => container.resolve(_import));
